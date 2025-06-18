eff96670a530c5796a6d08143eb9096cfd8313fb
cynthia Obure <cynthia.obure@ke.cicinsurancegroup.com>
2025-06-18 16:25:40 +0300

    commit

--- script.js
+++ script.js
@@ -8,5 +8,100 @@ document.addEventListener("DOMContentLoaded", () => {
     slides[current].classList.add("active");
   }
 
-  setInterval(showNextSlide, 3000); // Change every 3 seconds
+  setInterval(showNextSlide, 3000); 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const counters = document.querySelectorAll('.stat h2');
+//   let hasCounted = false;
+
+//   function animateCount() {
+//     counters.forEach(counter => {
+//       const target = parseFloat(counter.getAttribute('data-target'));
+//       const isDecimal = target % 1 !== 0;
+//       let current = 0;
+//       const increment = target / 100;
+
+//       const updateCount = () => {
+//         current += increment;
+//         if (current < target) {
+//           counter.innerText = isDecimal ? current.toFixed(2) : Math.ceil(current);
+//           requestAnimationFrame(updateCount);
+//         } else {
+//           counter.innerText = isDecimal ? target.toFixed(2) : target;
+//         }
+//       };
+
+//       updateCount();
+//     });
+//   }
+
+//   function handleScroll() {
+//     const statsSection = document.getElementById('stats');
+//     const sectionTop = statsSection.getBoundingClientRect().top;
+//     const windowHeight = window.innerHeight;
+
+//     if (sectionTop < windowHeight && !hasCounted) {
+//       animateCount();
+//       hasCounted = true;
+//     }
+//   }
+
+//   window.addEventListener('scroll', handleScroll);
